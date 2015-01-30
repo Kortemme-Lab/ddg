f1f5d649b7a02e5287653af5a40a6a4213fe2bf9
Kyle Barlow <kb@kylebarlow.com>
2015-01-29 22:41:40 -0800

    Changed .csv output format

--- protocols/alanine-scanning/analyze.py
+++ protocols/alanine-scanning/analyze.py
@@ -8,10 +8,10 @@ import sqlite3
 
 pickle_name = os.path.join('data', 'job_dict.pickle')
 
-def parse_db_output(db_output_file, ddg_data):
+def parse_db_output(db_output_file, ddg_data, score_fxns):
     query = 'SELECT ddG.resNum, ddg.mutated_to_name3, ddg.ddG_value, ' \
     'structures.tag, residue_pdb_identification.residue_number, ' \
-    'residue_pdb_identification.chain_id, residues.name3, batches.description ' \
+    'residue_pdb_identification.chain_id, residues.name3, batches.description, batches.name ' \
     'FROM ddg INNER JOIN structures ON structures.struct_id=ddg.struct_id ' \
     'INNER JOIN residue_pdb_identification ON residue_pdb_identification.struct_id=structures.struct_id ' \
     'AND residue_pdb_identification.residue_number=ddg.resNum ' \
@@ -22,13 +22,16 @@ def parse_db_output(db_output_file, ddg_data):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    for rosetta_resnum, mutated_to, ddg_calc, tag, pdb_resnum, chain, original_name3, description in c.execute(query):
+    for rosetta_resnum, mutated_to, ddg_calc, tag, pdb_resnum, chain, original_name3, description, tanja_id in c.execute(query):
         pdb_id = tag.split('_')[0]
         assert( len(pdb_id) == 4 )
-        ddg_data[(pdb_id, original_name3, pdb_resnum, chain, mutated_to, pdb_resnum, description)] = ddg_calc
+        if tanja_id not in ddg_data:
+            ddg_data[tanja_id] = {}
+        ddg_data[tanja_id][description] = ddg_calc
+        score_fxns.add(description)
 
     conn.close()
-    return ddg_data
+    return (ddg_data, score_fxns)
 
 if __name__ == '__main__':
 
@@ -38,7 +41,6 @@ if __name__ == '__main__':
                         help = 'Output directories')
     args = parser.parse_args()
 
-    ddg_data = {}
     for output_dir in args.output_dirs:
         assert( os.path.isdir(output_dir) )
         job_pickle_file = os.path.join(
@@ -57,12 +59,23 @@ if __name__ == '__main__':
 
         print 'Found %d output dbs (%d expected)' % (len(db_output_files), len(job_dict))
 
+        score_fxns = set()
+        ddg_data = {}
         for db_output_file in db_output_files:
-            ddg_data = parse_db_output(db_output_file, ddg_data)
+            ddg_data, score_fxns = parse_db_output(db_output_file, ddg_data, score_fxns)
 
-    print 'Processed %d data points' % len(ddg_data)
-    with open('results.csv', 'w') as f:
-        f.write('PDB,Mutation,score_function,ddg_calc\n')
-        for tup in ddg_data:
-            pdb_id, original_name3, pdb_resnum, chain, mutated_to, pdb_resnum, description = tup
-            f.write('%s%d,%s%d%s%s,%s,%.6f\n' % (pdb_id, pdb_resnum, original_name3, pdb_resnum, chain, mutated_to, description, ddg_data[tup]) )
+        score_fxns = sorted(list(score_fxns))
+        print 'Processed %d data points' % len(ddg_data)
+        with open('results-%s.csv' % os.path.basename(output_dir), 'w') as f:
+            f.write('ID')
+            for score_fxn in score_fxns:
+                f.write(',%s' % score_fxn)
+            f.write('\n')
+            for tanja_id in ddg_data:
+                f.write('%s' % tanja_id)
+                for score_fxn in score_fxns:
+                    if score_fxn in ddg_data[tanja_id]:
+                        f.write(',%.6f' % ddg_data[tanja_id][score_fxn])
+                    else:
+                        f.write(',NA')
+                f.write('\n')
