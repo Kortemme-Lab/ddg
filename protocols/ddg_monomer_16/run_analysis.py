b51287516a3839dc9d715d9aa6a2503c333c6d40
Shane O'Connor <spiderbaby@gmail.com>
2015-09-22 12:33:23 -0700

    Switching the copyright author order to match the order below (sorry, Kyle!)

--- protocols/ddg_monomer_16/run_analysis.py
+++ protocols/ddg_monomer_16/run_analysis.py
@@ -2,7 +2,7 @@
 
 # The MIT License (MIT)
 #
-# Copyright (c) 2015 Kyle A. Barlow, Shane O'Connor
+# Copyright (c) 2015 Shane O'Connor, Kyle A. Barlow
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
